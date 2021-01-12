dc531ad084a2c16bbdbc7ea7fe38673b4619ceac
jahan <jahan@j-MacBook-Air.home>
2021-01-12 18:36:15 -0500

    Convert App function  component to the class component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './images.jpeg';
 import './App.css';
 import Login from './components/Login';
+import React from 'react';
 
-function App() {
+class App extends React.Component {
+  render(){
   return (
     <div className="App">
       <img src={logo} alt="bereketli Turkmen" id="logo" />
       <hr></hr>
       <Login />
     </div>
-  );
-}
+   );
+ }
+} 
 
 export default App;
