59d0f82472a299b292001a257006cbb56c83d460
jahan <jahan@j-MacBook-Air.home>
2021-01-12 23:03:10 -0500

    Add login functionality

--- src/App.js
+++ src/App.js
@@ -4,12 +4,54 @@ import Login from './components/Login';
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      username:'',
+      password:'',
+      loggedIn:false
+    };
+    this.usercheck = this.usercheck.bind(this);
+    this.setUserName = this.setUserName.bind(this);
+    this.setPassword = this.setPassword.bind(this);
+  }
+
+  usercheck() {
+    if(this.state.username==="abc" && this.state.password==="123"){
+      this.setState({
+        loggedIn:true
+      })
+    }else{
+      alert('Username and Password doesn\'t match');
+    }
+  }
+
+  setUserName(UserName) {
+    this.setState({
+      username:UserName
+    })
+  }
+
+  setPassword(UserPassword) {
+    this.setState({
+      password: UserPassword
+    })
+  }
+
   render(){
-  return (
+
+    let currComponent = <div></div>;
+    if(this.state.loggedIn) {
+      currComponent = <h1>You are successfully logged in</h1>
+    } else {
+      currComponent = <Login userName={this.setUserName} UserPassword={this.setPassword} checkUser={this.usercheck}/>
+    }
+  
+    return (
     <div className="App">
       <img src={logo} alt="bereketli Turkmen" id="logo" />
       <hr></hr>
-      <Login />
+      {currComponent}
     </div>
    );
  }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,22 @@
-function Login() {
+function Login(props) {
+  function handleUsername(e) {
+    props.userName(e.target.value);
+  }
+
+  function handlePassword(e) {
+    props.UserPassword(e.target.value);
+  }
+
   return (
     <form>
       <h1>Login to start creating your order!</h1>
       <label htmlFor="username">Username: </label>
-      <input type="text" name="username" id="username" autoComplete="off" />
+      <input type="text" name="username" id="username" autoComplete="off" onInput={handleUsername}/>
       <br/>
       <label htmlFor="user-pw">Password: </label>
-      <input type="password" name="user-pw" id="user-pw" autoComplete="off" />
+      <input type="password" name="user-pw" id="user-pw" autoComplete="off" onInput={handlePassword}/>
       <br/>
-      <input type="button" value="Login"/>
+      <input type="button" value="Login" onClick={props.checkUser}/>
     </form>
   )
 }
